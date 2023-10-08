ff0e99a07611e6fe0cf156f2bee489af7868301a
Vu Ngoc Ngoai Thuong <thuonglun16102@gmail.com>
2023-10-08 11:29:06 +0700

    Buoi 3

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit683908c0db546f072fca74e5f51990bb
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit683908c0db546f072fca74e5f51990bb', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit683908c0db546f072fca74e5f51990bb', 'loadClassLoader'));
